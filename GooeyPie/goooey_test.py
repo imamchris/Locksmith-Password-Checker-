f81dd660ee2c2ceb4fe21a3ef47a01b0308d44be
cpereira <cpereira@student.saintaug.nsw.edu.au>
2024-05-16 12:00:06 +1000

    Improved and tested on code

--- GooeyPie/goooey_test.py
+++ GooeyPie/goooey_test.py
@@ -1,18 +1,49 @@
 import gooeypie as gp
 
-def say_hello(event): # triggers an event
-    hello_lbl.text = 'Hello Gooey Pie!'
+# def say_hello(event): # triggers an event
+#     hello_lbl.text = 'Hello Gooey Pie!'
 
 
-app = gp.GooeyPieApp('Hello!')
-app.width = 250
+# app = gp.GooeyPieApp('Hello!')
+# app.width = 250
 
-hello_btn = gp.Button(app, 'Say Hello', say_hello) # ___ - Text - Event
-hello_lbl = gp.Label(app, '')
+# hello_btn = gp.Button(app, 'Say Hello', say_hello) # ___ - Text - Event
+# hello_lbl = gp.Label(app, '')
 
-app.set_grid(10, 10) # Size of Tab
-app.add(hello_btn, 1, 1, align='center')
-app.add(hello_lbl, 2, 1, align='center') # Text - Location: y, x (make sure it's within size of the Tab) - alignment
+# app.set_grid(10, 10) # Size of Tab
+# # app.add(hello_btn, 1, 1, align='center')
+# # app.add(hello_lbl, 2, 1, align='center') # Text - Location: y, x (make sure it's within size of the Tab) - alignment
+
+
+# app.add(hello_btn, 5, 5, align= 'center', fill = True)
+
+app = gp.GooeyPieApp('TabContainer Demo')
+
+# Create tab container
+tabs_cont = gp.TabContainer(app)
+
+# Create tabs
+tab1_tab = gp.Tab(tabs_cont, 'Tab 1')
+tab2_tab = gp.Tab(tabs_cont, 'Tab 2')
+
+# Create widgets for each tab
+some_lbl = gp.Label(tab1_tab, 'A label in Tab 1')
+some_btn = gp.Button(tab2_tab, 'A button in Tab 2', None)
+
+# Add widgets to each tab
+tab1_tab.set_grid(1, 1)
+tab1_tab.add(some_lbl, 1, 1, align='center', valign='middle')
+tab2_tab.set_grid(1, 1)
+tab2_tab.add(some_btn, 1, 1, align='center', valign='middle')
+
+# Add tabs to TabContainer
+tabs_cont.add(tab1_tab)
+tabs_cont.add(tab2_tab)
+
+# Add TabContainer to main window
+app.set_grid(1, 1)
+app.add(tabs_cont, 1, 1, fill=True, stretch=True)
+
 
 
 app.run()
--- password_checker.py
+++ password_checker.py
@@ -1,7 +1,16 @@
 import gooeypie as gp
 
 def say_hello(event): # triggers an event
-    greeting_lbl.text = 'Hello Gooey Pie!'
+    #startup_lbl.text = ''
+    # app.add(name_lbl, 11, 5, align='center')
+    # app.add(name_inp, 12, 5)
+    # app.add(hello_btn, 13, 5, align='center')
+    # app.add(hello_lbl, 14, 5, align='center')
+
+
+
+def password_input(event):
+    print("eh")
 
 app = gp.GooeyPieApp('Locksmith')
 
@@ -9,13 +18,26 @@ app.width = 250
 
 hello_lbl = gp.Label(app, 'Hello! Welcome to Locksmith!')
 start_lbl = gp.Label(app, 'Please press the button to begin')
-greeting_btn = gp.Button(app, 'Begin', say_hello)
-greeting_lbl = gp.Label(app, '')
+startup_btn = gp.Button(app, 'Begin', say_hello)
+startup_lbl = gp.Label(app, '')
 
-app.set_grid(10, 10) # Size of Tab
+name_lbl = gp.Label(app, 'What is the password?')
+name_inp = gp.Input(app)
+name_inp.justify = 'center'
+name_inp.width = 30
+hello_btn = gp.Button(app, 'Begin', password_input)
+hello_lbl = gp.Label(app, '')
+
+app.set_grid(30, 30) # Size of Tab
 app.add(hello_lbl, 5, 5, align='center') # Text - Location: y, x (make sure it's within size of the Tab) - alignment
 app.add(start_lbl, 7, 5, align='center')
-app.add(greeting_btn, 9, 5, align='center')
-app.add(greeting_lbl, 10, 5, align='center')
+app.add(startup_btn, 9, 5, align='center')
+app.add(startup_lbl, 10, 5, align='left')
+
+app.add(name_lbl, 11, 5, align='center')
+app.add(name_inp, 12, 5)
+app.add(hello_btn, 13, 5, align='center')
+app.add(hello_lbl, 14, 5, align='center')
+
 
 app.run()
