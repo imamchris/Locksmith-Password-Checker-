922e3c09ada8af3bf245d5618584d3d969cd5daf
cpereira <cpereira@student.saintaug.nsw.edu.au>
2024-05-21 10:31:01 +1000

    Allowed for the password to be hidden and to be toggled on or off

--- GooeyPie/goooey_test.py
+++ GooeyPie/goooey_test.py
@@ -17,34 +17,50 @@ import gooeypie as gp
 
 # app.add(hello_btn, 5, 5, align= 'center', fill = True)
 
-app = gp.GooeyPieApp('TabContainer Demo')
+# app = gp.GooeyPieApp('TabContainer Demo')
 
 # Create tab container
-tabs_cont = gp.TabContainer(app)
+# tabs_cont = gp.TabContainer(app)
 
-# Create tabs
-tab1_tab = gp.Tab(tabs_cont, 'Tab 1')
-tab2_tab = gp.Tab(tabs_cont, 'Tab 2')
+# # Create tabs
+# tab1_tab = gp.Tab(tabs_cont, 'Tab 1')
+# tab2_tab = gp.Tab(tabs_cont, 'Tab 2')
 
-# Create widgets for each tab
-some_lbl = gp.Label(tab1_tab, 'A label in Tab 1')
-some_btn = gp.Button(tab2_tab, 'A button in Tab 2', None)
+# # Create widgets for each tab
+# some_lbl = gp.Label(tab1_tab, 'A label in Tab 1')
+# some_btn = gp.Button(tab2_tab, 'A button in Tab 2', None)
 
-# Add widgets to each tab
-tab1_tab.set_grid(1, 1)
-tab1_tab.add(some_lbl, 1, 1, align='center', valign='middle')
-tab2_tab.set_grid(1, 1)
-tab2_tab.add(some_btn, 1, 1, align='center', valign='middle')
+# # Add widgets to each tab
+# tab1_tab.set_grid(1, 1)
+# tab1_tab.add(some_lbl, 1, 1, align='center', valign='middle')
+# tab2_tab.set_grid(1, 1)
+# tab2_tab.add(some_btn, 1, 1, align='center', valign='middle')
 
-# Add tabs to TabContainer
-tabs_cont.add(tab1_tab)
-tabs_cont.add(tab2_tab)
+# # Add tabs to TabContainer
+# tabs_cont.add(tab1_tab)
+# tabs_cont.add(tab2_tab)
 
-# Add TabContainer to main window
-app.set_grid(1, 1)
-app.add(tabs_cont, 1, 1, fill=True, stretch=True)
+# # Add TabContainer to main window
+# app.set_grid(1, 1)
+# app.add(tabs_cont, 1, 1, fill=True, stretch=True)
 
+def toggle_mask(event):
+    secret.toggle()
 
+app = gp.GooeyPieApp('Secret')
+
+question = gp.Label(app, "What's your secret?")
+
+secret = gp.Secret(app)
+secret.width = 50
+
+check = gp.Checkbox(app, 'Show secret')
+check.add_event_listener('change', toggle_mask)
+
+app.set_grid(3, 1)
+app.add(question, 1, 1)
+app.add(secret, 2, 1)
+app.add(check, 3, 1)
 
 app.run()
 
--- password_checker.py
+++ password_checker.py
@@ -1,48 +1,51 @@
 import gooeypie as gp
 
+def toggle_mask(event):
+    checker_inp.toggle()
+
 def password_check(event):
     
-    if len(checker_inp.text) == 5:
+    if len(checker_inp.text) == 0:
+        verify_lbl.text = 'Please enter your password!' 
+    elif len(checker_inp.text) >= 5:
         verify_lbl.text = 'GREAT!' 
-    else:
-        verify_lbl.text = 'Could be better...'
+    elif len(checker_inp.text) <= 5:
+        verify_lbl.text = 'Your password should be 5 characters long atleast' 
     
     checker_inp.update()
 
-app = gp.GooeyPieApp('Locksmith')
-# tabs_cont = gp.TabContainer(app)
 
-# intro = gp.Tab(tabs_cont, 'Tab 1')
-# checker = gp.Tab(tabs_cont, 'Tab 2')
+
+app = gp.GooeyPieApp('Locksmith')
 
 app.set_size(250, 250) 
 
+check = gp.Checkbox(app, 'Show secret')
+check.add_event_listener('change', toggle_mask)
+
 intro_lbl = gp.Label(app, 'Hello! Welcome to Locksmith!') # location, text
 ask_lbl = gp.Label(app, 'Enter your password...')
-verify_lbl = gp.Label(app, 'NAN')
-checker_inp = gp.Input(app)
-checker_inp.justify = 'center'
-checker_inp.width = 30
+verify_lbl = gp.Label(app, '')
+checker_inp = gp.Secret(app)
+checker_inp.justify = 'left'
+checker_inp.width = 45
 checker_btn = gp.Button(app, 'Check', password_check) # location, text, function
 
-# intro_tab.set_grid(1, 1)
-# checker_tab.add(some_lbl, 1, 1, align='center', valign='middle')
 
-app.set_grid(50, 50) # Size of Tab
-# intro.set_grid(50, 50)
 
-app.add(intro_lbl, 5, 25, align='center') # Text - Location: y, x (make sure it's within size of the Tab) - alignment
+app.set_grid(50, 200) # Size of Tab
+
+app.add(intro_lbl, 5, 25, align='left') # Text - Location: y, x (make sure it's within size of the Tab) - alignment
+
+
 
 app.set_grid(50, 50)
-app.add(ask_lbl, 7, 25, align='center')
+app.add(ask_lbl, 7, 25, align='left')
 app.add(checker_inp, 10, 25)
-app.add(verify_lbl, 12, 25, align='center')
-app.add(checker_btn, 17, 25, align='center')
+app.add(check, 11, 25, allign ='left') 
+app.add(verify_lbl, 12, 25, align='left')
+app.add(checker_btn, 13, 25, align='left')
 
-# tabs_cont.add(intro)
-# tabs_cont.add(checker)
-
-# app.add(tabs_cont, 1, 1, fill=True, stretch=True)
 
 
 app.run()
